75b06779c65cd23779070a5046564073cb845893
Brendon Oleary <brendon@seatgeek.com>
2018-08-26 10:36:58 -0400

    create a separate struct for a password iterator and create a pipe method on the PasswordGenerator trait for chaining

--- src/generators/base.rs
+++ src/generators/base.rs
@@ -1,9 +1,10 @@
+use std::str::FromStr;
 
 
-pub struct ChainedGenerator<'a> {
-    first: Box<dyn PasswordGenerator + 'a>,
-    second: Box<dyn PasswordGenerator + 'a>,
-}
+pub static ASCII_LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
+pub static ASCII_UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+pub static DIGITS: &str = "0123456789";
+
 
 pub trait PasswordGenerator {
     fn generate_with_seed(&mut self, seed: String) -> String {
@@ -12,7 +13,7 @@ pub trait PasswordGenerator {
     fn generate(&mut self) -> String {
         self.generate_with_seed(String::new())
     }
-    fn chain<'a, T>(self, other: T) -> ChainedGenerator<'a>
+    fn pipe<'a, T>(self, other: T) -> ChainedGenerator<'a>
         where
             Self: Sized + 'a,
             T: PasswordGenerator + Sized + 'a
@@ -22,11 +23,47 @@ pub trait PasswordGenerator {
             second: Box::new(other),
         }
     }
+    fn iterator<'a>(self) -> PasswordIterator<'a>
+        where Self: Sized + 'a
+    {
+        PasswordIterator { generator: Box::new(self) }
+    }
+}
+
+pub struct ChainedGenerator<'a> {
+    first: Box<dyn PasswordGenerator + 'a>,
+    second: Box<dyn PasswordGenerator + 'a>,
 }
 
 impl<'a> PasswordGenerator for ChainedGenerator<'a> {
     fn generate_with_seed(&mut self, seed: String) -> String {
         let seed = self.first.generate_with_seed(seed);
         self.second.generate_with_seed(seed)
+    }
+}
+
+pub struct Constant { value: String }
+
+impl Constant {
+    pub fn new(seed: &str) -> Constant {
+        let value = String::from_str(&seed).unwrap();
+        Constant { value }
+    }
+}
+
+impl PasswordGenerator for Constant {
+    fn generate_with_seed(&mut self, seed: String) -> String {
+        seed + &self.value
+    }
+}
+
+pub struct PasswordIterator<'a> { generator: Box<dyn PasswordGenerator + 'a> }
+
+impl<'a> Iterator for PasswordIterator<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let password = self.generator.generate();
+        Some(password)
     }
 }
