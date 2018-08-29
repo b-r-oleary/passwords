4e609e6e9ea85db3086b668e510459d788c60580
Brendon Oleary <brendon@seatgeek.com>
2018-08-28 22:24:17 -0400

    update the main script and import the primary objects to the top level of the library

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@ extern crate rand;
 extern crate inflector;
 
 pub mod generators;
+
+pub use generators::base::{PasswordGenerator, Constant};
+pub use generators::case::Case;
+pub use generators::defects::Defects;
+pub use generators::phrase::{RandomPhrases, RandomWords};
+pub use generators::random_string::RandomString;
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 extern crate passwords;
 
 use std::io;
+use std::io::Write;
 
-use passwords::generators::base::{Constant, PasswordGenerator};
-use passwords::generators::base::{ASCII_LOWERCASE, ASCII_UPPERCASE, DIGITS};
-use passwords::generators::case::Case;
-use passwords::generators::phrase::{RandomPhrases, RandomWords};
-use passwords::generators::random_string::RandomString;
+use passwords::PasswordGenerator;
+use passwords::{Constant, Case, RandomPhrases, RandomString, Defects};
 
 
 fn main() {
     let passwords = Constant::new("")
         .pipe(RandomPhrases::new("./texts/alice-in-wonderland.txt", 3, 5))
         .pipe(Case::Class)
-        .pipe(RandomString::new(2).with_characters(DIGITS.chars().collect()));
-        // .pipe(
-        //     RandomString::new(2)
-        //     .with_characters(DIGITS.chars().collect()))
+        .pipe(RandomString::digits(2))
+        .pipe(Defects::with_symbols(1, 1))
+        .pipe(Defects::with_vowels(1, 1));
 
     for password in passwords.iterator().into_iter() {
-        println!("> {}", password);
+        println!("{}", password);
 
         let mut input = String::new();
+
+        print!("> ");
+        io::stdout().flush().unwrap();
 
         match io::stdin().read_line(&mut input) {
             Ok(_) if input.contains("exit") => break,
