df991c8d5b3ed66cc6f4f4b762aa7a07bc674a94
Brendon Oleary <brendon@seatgeek.com>
2018-09-03 17:23:50 -0400

    create two functions that generate default implementations of password generator objects

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,26 @@ extern crate inflector;
 
 pub mod generators;
 
-pub use generators::base::{PasswordGenerator, Constant};
+pub use generators::base::{PasswordGenerator, ChainedGenerator, Constant};
 pub use generators::case::Case;
 pub use generators::defects::Defects;
-pub use generators::phrase::{RandomPhrases, RandomWords};
+pub use generators::phrase::{RandomPhrases, RandomWords, Text};
 pub use generators::random_string::RandomString;
+
+
+pub fn phrase_passwords<'a>() -> ChainedGenerator<'a> {
+    Constant::new("")
+        .pipe(RandomPhrases::from_text(&Text::the_time_machine(), 3, 5))
+        .pipe(Case::Class)
+        .pipe(RandomString::digits(2))
+        .pipe(Defects::with_symbols(1, 1))
+        .pipe(Defects::with_vowels(1, 1))
+}
+
+pub fn xkcd_passwords<'a>() -> ChainedGenerator<'a> {
+    Constant::new("")
+        .pipe(
+            RandomWords::from_text(&Text::nouns(), 4, 4)
+            .or(RandomWords::from_text(&Text::the_time_machine(), 4, 5))
+        )
+}
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,11 @@ extern crate passwords;
 use std::io;
 use std::io::Write;
 
-use passwords::PasswordGenerator;
-use passwords::{Constant, Case, RandomPhrases, RandomString, Defects};
+use passwords::{PasswordGenerator, xkcd_passwords};
 
 
 fn main() {
-    let passwords = Constant::new("")
-        .pipe(RandomPhrases::new("./texts/alice-in-wonderland.txt", 3, 5))
-        .pipe(Case::Class)
-        .pipe(RandomString::digits(2))
-        .pipe(Defects::with_symbols(1, 1))
-        .pipe(Defects::with_vowels(1, 1));
-
-    for password in passwords.iterator().into_iter() {
+    for password in xkcd_passwords().iterator().into_iter() {
         println!("{}", password);
 
         let mut input = String::new();
