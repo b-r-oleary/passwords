a67fc2f0f9cd44babb276247b1fbe3b36967c993
Brendon Oleary <brendon@seatgeek.com>
2020-12-28 14:28:29 -0500

    switch from clap to structopt

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,53 @@
-extern crate clap;
-extern crate passwords;
-
-use std::io;
-use std::io::Write;
-
-use clap::{App, Arg};
+use anyhow::{anyhow, Error, Result};
+use std::str::FromStr;
+use structopt::StructOpt;
 
 use passwords::{phrase_passwords, xkcd_passwords, ChainedGenerator, PasswordGenerator};
 
-struct Config<'a> {
-    generator: ChainedGenerator<'a>,
+/// A tool for generating memorable, high entropy passwords
+#[derive(StructOpt, Debug)]
+#[structopt(name = "passwords")]
+struct Opt {
+    /// The number of password samples to generate.
+    #[structopt(short, long, default_value = "1")]
+    n_samples: usize,
+
+    /// The type of generator to use to generate passwords.
+    #[structopt(short, long, default_value = "phrases")]
+    generator_type: GeneratorType,
 }
 
-impl<'a> Config<'a> {
-    fn get_generator(name: &str) -> Result<ChainedGenerator<'a>, String> {
-        match name {
-            "xkcd" => Ok(xkcd_passwords()),
-            "phrases" => Ok(phrase_passwords()),
-            _ => Err(format!("invalid generator name: {}", name)),
+#[derive(StructOpt, Debug)]
+enum GeneratorType {
+    XKCD,
+    Phrases,
+}
+
+impl FromStr for GeneratorType {
+    type Err = Error;
+
+    fn from_str(input: &str) -> Result<Self> {
+        match input {
+            "xkcd" => Ok(GeneratorType::XKCD),
+            "phrases" => Ok(GeneratorType::Phrases),
+            _ => Err(anyhow!("Did not recognize '{}' as a generator type", input)),
         }
     }
+}
 
-    fn parse() -> Result<Config<'a>, String> {
-        let generator_names = ["phrases", "xkcd"];
-
-        let matches = App::new("Password Generator")
-            .arg(
-                Arg::with_name("generator")
-                    .short("g")
-                    .long("generator")
-                    .takes_value(true)
-                    .help(&format!(
-                        "specifies a password generator, one of {:?}",
-                        generator_names
-                    ))
-                    .possible_values(&generator_names),
-            )
-            .get_matches();
-
-        let generator_name = matches.value_of("generator").unwrap_or(generator_names[0]);
-
-        let generator = Self::get_generator(generator_name)?;
-
-        Ok(Config { generator })
+impl Opt {
+    fn get_generator<'a>(&'a self) -> ChainedGenerator<'a> {
+        match self.generator_type {
+            GeneratorType::XKCD => xkcd_passwords(),
+            GeneratorType::Phrases => phrase_passwords(),
+        }
     }
 }
 
 fn main() {
-    let config = Config::parse().unwrap();
-
-    for password in config.generator.iterator().into_iter() {
+    let opts = Opt::from_args();
+    let generator = opts.get_generator();
+    for password in generator.iterator().into_iter().take(opts.n_samples) {
         println!("{}", password);
-
-        let mut input = String::new();
-
-        print!("> ");
-        io::stdout().flush().unwrap();
-
-        match io::stdin().read_line(&mut input) {
-            Ok(_) if input.contains("exit") => {
-                println!("Bye!");
-                break;
-            }
-            Ok(_) => continue,
-            Err(error) => println!("error: {}", error),
-        }
     }
 }
